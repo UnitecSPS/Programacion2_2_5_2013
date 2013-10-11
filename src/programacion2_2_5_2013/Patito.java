2b312e5a10f401a1e0b5d4bd4c44d53745dcf01e
Carlos Gochez <carlos.gochez@unitec.edu>
2013-10-11 08:56:40 -0600

    Agregue patito waka waka

--- src/programacion2_2_5_2013/Patito.java
+++ src/programacion2_2_5_2013/Patito.java
@@ -8,13 +8,8 @@ package programacion2_2_5_2013;
  *
  * @author Gotcha
  */
-public class Programacion2_2_5_2013 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Patito {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("WAKA WAKA");
+        System.out.println("Masiso!");
     }
 }
